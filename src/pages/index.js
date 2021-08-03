28342858988ab4201fdf0d71d3f37e8415774b1c
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-08-03 02:29:57 +0100

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function IndexPage() {
         <AppFeature />
         <Dashboard />
         <Pricing />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Subscribe />
       </Layout>
     </ThemeProvider>

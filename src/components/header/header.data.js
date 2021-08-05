d4d4a682204f56fa5307848da230da83e3092d5f
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-08-05 03:02:52 +0100

    more

--- src/components/header/header.data.js
+++ src/components/header/header.data.js
@@ -16,8 +16,8 @@ export default [
     path: 'pricing',
     label: 'Pricing',
   },
-  {
-    path: 'testimonials',
-    label: 'Testimonials',
-  },
+  // {
+  //   path: 'testimonials',
+  //   label: 'Testimonials',
+  // },
 ];
